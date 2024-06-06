ee46e3aa57813eb5abc6a5ab8c303a66d20794cf
adilzhanshukenov <shukenovadilzhan@gmail.com>
2024-06-06 20:29:49 +0500

    Count rolls and track time added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,17 @@ function App() {
 
   const [dice, setDice] = useState(allNewDice())
   const [tenzies, setTenzies] = useState(false)
+  const [countRolls, setCountRolls] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+
+  function resetGame() {
+    setSeconds(0)
+    setIsActive(false)
+    setTenzies(false)
+    setDice(allNewDice())
+    setCountRolls(0)
+  }
 
   function allNewDice() {
     const diceArray = []
@@ -30,9 +41,9 @@ function App() {
         {...die, id: nanoid(), value: randomNumber} :
         die
       }))
+      setCountRolls(prevCount => prevCount + 1)
     } else { 
-      setTenzies(false)
-      setDice(allNewDice())
+      resetGame()
     }
   }
 
@@ -44,6 +55,7 @@ function App() {
         die}
       )
     )
+    setIsActive(true)
   }
 
   useEffect(() => {
@@ -54,9 +66,23 @@ function App() {
 
     if(allEqualAndHeld(dice)) {
       setTenzies(true)
+      setIsActive(false)
       console.log("You won!")
     }
   }, [dice])
+
+  useEffect(() => {
+
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
 
   const diceElements = dice.map(die => 
     <Die 
@@ -70,6 +96,10 @@ function App() {
   return (
     <main className='main'>
       {tenzies && <Confetti />}
+      <div className='rolls-sec'>
+          <h3>Rolls: {countRolls}</h3>
+          <h3>Time: {seconds}</h3>
+        </div>
       <h1>Tenzies</h1>
       <div className='main--text'>
         <h2>Roll until all dice are the same. 
